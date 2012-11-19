5b91c0efc657389ed28e425587d5d52a9834c9f4
Floris Verburg <floris.verburg@gmail.com>
2012-11-19 12:20:07 +0100

    cookie in bootstrap

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -63,6 +63,8 @@ ini_set('unserialize_callback_func', 'spl_autoload_call');
  */
 I18n::lang('en-us');
 
+Cookie::$salt = 'asdf';
+
 /**
  * Set Kohana::$environment if a 'KOHANA_ENV' environment variable has been supplied.
  *
--- application/logs/2012/11/19.php
+++ application/logs/2012/11/19.php
@@ -63,3 +63,18 @@
 #5 /Users/Sander/djpatty/system/classes/Kohana/Request.php(990): Kohana_Request_Client->execute(Object(Request))
 #6 /Users/Sander/djpatty/index.php(118): Kohana_Request->execute()
 #7 {main} in /Users/Sander/djpatty/application/classes/Controller/Hello.php:9
+2012-11-19 12:12:42 --- EMERGENCY: Kohana_Exception [ 0 ]: A valid cookie salt is required. Please set Cookie::$salt. ~ SYSPATH/classes/Kohana/Cookie.php [ 152 ] in /Users/Floris/git/djpatty/system/classes/Kohana/Cookie.php:67
+2012-11-19 12:12:42 --- DEBUG: #0 /Users/Floris/git/djpatty/system/classes/Kohana/Cookie.php(67): Kohana_Cookie::salt('SQLiteManager_c...', NULL)
+#1 /Users/Floris/git/djpatty/system/classes/Kohana/Request.php(155): Kohana_Cookie::get('SQLiteManager_c...')
+#2 /Users/Floris/git/djpatty/index.php(117): Kohana_Request::factory(true, Array, false)
+#3 {main} in /Users/Floris/git/djpatty/system/classes/Kohana/Cookie.php:67
+2012-11-19 12:12:49 --- EMERGENCY: Kohana_Exception [ 0 ]: A valid cookie salt is required. Please set Cookie::$salt. ~ SYSPATH/classes/Kohana/Cookie.php [ 152 ] in /Users/Floris/git/djpatty/system/classes/Kohana/Cookie.php:67
+2012-11-19 12:12:49 --- DEBUG: #0 /Users/Floris/git/djpatty/system/classes/Kohana/Cookie.php(67): Kohana_Cookie::salt('SQLiteManager_c...', NULL)
+#1 /Users/Floris/git/djpatty/system/classes/Kohana/Request.php(155): Kohana_Cookie::get('SQLiteManager_c...')
+#2 /Users/Floris/git/djpatty/index.php(117): Kohana_Request::factory(true, Array, false)
+#3 {main} in /Users/Floris/git/djpatty/system/classes/Kohana/Cookie.php:67
+2012-11-19 12:14:31 --- EMERGENCY: Kohana_Exception [ 0 ]: A valid cookie salt is required. Please set Cookie::$salt. ~ SYSPATH/classes/Kohana/Cookie.php [ 152 ] in /Users/Floris/git/djpatty/system/classes/Kohana/Cookie.php:67
+2012-11-19 12:14:31 --- DEBUG: #0 /Users/Floris/git/djpatty/system/classes/Kohana/Cookie.php(67): Kohana_Cookie::salt('SQLiteManager_c...', NULL)
+#1 /Users/Floris/git/djpatty/system/classes/Kohana/Request.php(155): Kohana_Cookie::get('SQLiteManager_c...')
+#2 /Users/Floris/git/djpatty/index.php(117): Kohana_Request::factory(true, Array, false)
+#3 {main} in /Users/Floris/git/djpatty/system/classes/Kohana/Cookie.php:67
