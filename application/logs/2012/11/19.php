bd1e9317d8f6e39d465273182136d19578005280
Sander Ploegsma <sanderploegsma@gmail.com>
2012-11-19 12:11:07 +0100

    Hello World tutorial

---
+++ application/logs/2012/11/19.php
@@ -0,0 +1,65 @@
+<?php defined('SYSPATH') OR die('No direct script access.'); ?>
+
+2012-11-19 12:01:47 --- EMERGENCY: ErrorException [ 2 ]: Attempt to assign property of non-object ~ APPPATH/classes/Controller/Hello.php [ 9 ] in /Users/Sander/djpatty/application/classes/Controller/Hello.php:9
+2012-11-19 12:01:47 --- DEBUG: #0 /Users/Sander/djpatty/application/classes/Controller/Hello.php(9): Kohana_Core::error_handler(2, 'Attempt to assi...', '/Users/Sander/d...', 9, Array)
+#1 /Users/Sander/djpatty/system/classes/Kohana/Controller.php(84): Controller_Hello->action_index()
+#2 [internal function]: Kohana_Controller->execute()
+#3 /Users/Sander/djpatty/system/classes/Kohana/Request/Client/Internal.php(97): ReflectionMethod->invoke(Object(Controller_Hello))
+#4 /Users/Sander/djpatty/system/classes/Kohana/Request/Client.php(114): Kohana_Request_Client_Internal->execute_request(Object(Request), Object(Response))
+#5 /Users/Sander/djpatty/system/classes/Kohana/Request.php(990): Kohana_Request_Client->execute(Object(Request))
+#6 /Users/Sander/djpatty/index.php(118): Kohana_Request->execute()
+#7 {main} in /Users/Sander/djpatty/application/classes/Controller/Hello.php:9
+2012-11-19 12:03:31 --- EMERGENCY: ErrorException [ 2 ]: Attempt to assign property of non-object ~ APPPATH/classes/Controller/Hello.php [ 9 ] in /Users/Sander/djpatty/application/classes/Controller/Hello.php:9
+2012-11-19 12:03:31 --- DEBUG: #0 /Users/Sander/djpatty/application/classes/Controller/Hello.php(9): Kohana_Core::error_handler(2, 'Attempt to assi...', '/Users/Sander/d...', 9, Array)
+#1 /Users/Sander/djpatty/system/classes/Kohana/Controller.php(84): Controller_Hello->action_index()
+#2 [internal function]: Kohana_Controller->execute()
+#3 /Users/Sander/djpatty/system/classes/Kohana/Request/Client/Internal.php(97): ReflectionMethod->invoke(Object(Controller_Hello))
+#4 /Users/Sander/djpatty/system/classes/Kohana/Request/Client.php(114): Kohana_Request_Client_Internal->execute_request(Object(Request), Object(Response))
+#5 /Users/Sander/djpatty/system/classes/Kohana/Request.php(990): Kohana_Request_Client->execute(Object(Request))
+#6 /Users/Sander/djpatty/index.php(118): Kohana_Request->execute()
+#7 {main} in /Users/Sander/djpatty/application/classes/Controller/Hello.php:9
+2012-11-19 12:03:33 --- EMERGENCY: ErrorException [ 2 ]: Attempt to assign property of non-object ~ APPPATH/classes/Controller/Hello.php [ 9 ] in /Users/Sander/djpatty/application/classes/Controller/Hello.php:9
+2012-11-19 12:03:33 --- DEBUG: #0 /Users/Sander/djpatty/application/classes/Controller/Hello.php(9): Kohana_Core::error_handler(2, 'Attempt to assi...', '/Users/Sander/d...', 9, Array)
+#1 /Users/Sander/djpatty/system/classes/Kohana/Controller.php(84): Controller_Hello->action_index()
+#2 [internal function]: Kohana_Controller->execute()
+#3 /Users/Sander/djpatty/system/classes/Kohana/Request/Client/Internal.php(97): ReflectionMethod->invoke(Object(Controller_Hello))
+#4 /Users/Sander/djpatty/system/classes/Kohana/Request/Client.php(114): Kohana_Request_Client_Internal->execute_request(Object(Request), Object(Response))
+#5 /Users/Sander/djpatty/system/classes/Kohana/Request.php(990): Kohana_Request_Client->execute(Object(Request))
+#6 /Users/Sander/djpatty/index.php(118): Kohana_Request->execute()
+#7 {main} in /Users/Sander/djpatty/application/classes/Controller/Hello.php:9
+2012-11-19 12:03:33 --- EMERGENCY: ErrorException [ 2 ]: Attempt to assign property of non-object ~ APPPATH/classes/Controller/Hello.php [ 9 ] in /Users/Sander/djpatty/application/classes/Controller/Hello.php:9
+2012-11-19 12:03:33 --- DEBUG: #0 /Users/Sander/djpatty/application/classes/Controller/Hello.php(9): Kohana_Core::error_handler(2, 'Attempt to assi...', '/Users/Sander/d...', 9, Array)
+#1 /Users/Sander/djpatty/system/classes/Kohana/Controller.php(84): Controller_Hello->action_index()
+#2 [internal function]: Kohana_Controller->execute()
+#3 /Users/Sander/djpatty/system/classes/Kohana/Request/Client/Internal.php(97): ReflectionMethod->invoke(Object(Controller_Hello))
+#4 /Users/Sander/djpatty/system/classes/Kohana/Request/Client.php(114): Kohana_Request_Client_Internal->execute_request(Object(Request), Object(Response))
+#5 /Users/Sander/djpatty/system/classes/Kohana/Request.php(990): Kohana_Request_Client->execute(Object(Request))
+#6 /Users/Sander/djpatty/index.php(118): Kohana_Request->execute()
+#7 {main} in /Users/Sander/djpatty/application/classes/Controller/Hello.php:9
+2012-11-19 12:03:33 --- EMERGENCY: ErrorException [ 2 ]: Attempt to assign property of non-object ~ APPPATH/classes/Controller/Hello.php [ 9 ] in /Users/Sander/djpatty/application/classes/Controller/Hello.php:9
+2012-11-19 12:03:33 --- DEBUG: #0 /Users/Sander/djpatty/application/classes/Controller/Hello.php(9): Kohana_Core::error_handler(2, 'Attempt to assi...', '/Users/Sander/d...', 9, Array)
+#1 /Users/Sander/djpatty/system/classes/Kohana/Controller.php(84): Controller_Hello->action_index()
+#2 [internal function]: Kohana_Controller->execute()
+#3 /Users/Sander/djpatty/system/classes/Kohana/Request/Client/Internal.php(97): ReflectionMethod->invoke(Object(Controller_Hello))
+#4 /Users/Sander/djpatty/system/classes/Kohana/Request/Client.php(114): Kohana_Request_Client_Internal->execute_request(Object(Request), Object(Response))
+#5 /Users/Sander/djpatty/system/classes/Kohana/Request.php(990): Kohana_Request_Client->execute(Object(Request))
+#6 /Users/Sander/djpatty/index.php(118): Kohana_Request->execute()
+#7 {main} in /Users/Sander/djpatty/application/classes/Controller/Hello.php:9
+2012-11-19 12:03:34 --- EMERGENCY: ErrorException [ 2 ]: Attempt to assign property of non-object ~ APPPATH/classes/Controller/Hello.php [ 9 ] in /Users/Sander/djpatty/application/classes/Controller/Hello.php:9
+2012-11-19 12:03:34 --- DEBUG: #0 /Users/Sander/djpatty/application/classes/Controller/Hello.php(9): Kohana_Core::error_handler(2, 'Attempt to assi...', '/Users/Sander/d...', 9, Array)
+#1 /Users/Sander/djpatty/system/classes/Kohana/Controller.php(84): Controller_Hello->action_index()
+#2 [internal function]: Kohana_Controller->execute()
+#3 /Users/Sander/djpatty/system/classes/Kohana/Request/Client/Internal.php(97): ReflectionMethod->invoke(Object(Controller_Hello))
+#4 /Users/Sander/djpatty/system/classes/Kohana/Request/Client.php(114): Kohana_Request_Client_Internal->execute_request(Object(Request), Object(Response))
+#5 /Users/Sander/djpatty/system/classes/Kohana/Request.php(990): Kohana_Request_Client->execute(Object(Request))
+#6 /Users/Sander/djpatty/index.php(118): Kohana_Request->execute()
+#7 {main} in /Users/Sander/djpatty/application/classes/Controller/Hello.php:9
+2012-11-19 12:03:34 --- EMERGENCY: ErrorException [ 2 ]: Attempt to assign property of non-object ~ APPPATH/classes/Controller/Hello.php [ 9 ] in /Users/Sander/djpatty/application/classes/Controller/Hello.php:9
+2012-11-19 12:03:34 --- DEBUG: #0 /Users/Sander/djpatty/application/classes/Controller/Hello.php(9): Kohana_Core::error_handler(2, 'Attempt to assi...', '/Users/Sander/d...', 9, Array)
+#1 /Users/Sander/djpatty/system/classes/Kohana/Controller.php(84): Controller_Hello->action_index()
+#2 [internal function]: Kohana_Controller->execute()
+#3 /Users/Sander/djpatty/system/classes/Kohana/Request/Client/Internal.php(97): ReflectionMethod->invoke(Object(Controller_Hello))
+#4 /Users/Sander/djpatty/system/classes/Kohana/Request/Client.php(114): Kohana_Request_Client_Internal->execute_request(Object(Request), Object(Response))
+#5 /Users/Sander/djpatty/system/classes/Kohana/Request.php(990): Kohana_Request_Client->execute(Object(Request))
+#6 /Users/Sander/djpatty/index.php(118): Kohana_Request->execute()
+#7 {main} in /Users/Sander/djpatty/application/classes/Controller/Hello.php:9
