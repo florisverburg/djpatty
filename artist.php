3f38b8bca36f6a829ddb8c4b4073b66626c53204
Floris Verburg <floris.verburg@gmail.com>
2012-11-29 10:43:29 +0100

    template added

---
+++ artist.php
@@ -0,0 +1,313 @@
+<!DOCTYPE html>
+<html lang='en'>
+	<head>
+		<meta charset="utf-8">
+		<title>Beats by Dr. DJ Patty</title>
+		<meta name="description" content="DJ Patty">
+		<meta name="keywords" content="DJ, Patty">
+		<meta name="author" content="DJ Patty">
+		<link href="script/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+		<link href="style.css" rel="stylesheet" type="text/css">
+	</head>
+
+	<body>
+		<div class="container-fluid">
+			<header>
+				<img id = "logo" src="djpatty.png"/>
+			</header>
+			<div class="row-fluid">
+				<div class = "span8">
+					<div class="row-fluid">
+						<div id = "albums" class="span12">
+							<form method = "post" onsubmit ="return false;">
+								<fieldset>
+									<legend>
+										Albums
+									</legend>
+								</fieldset> 
+								<div id ="album">
+									<div id = "cover">
+										<img src = "http://userserve-ak.last.fm/serve/300x300/47226415.png"/>
+									</div>
+									<div id ="albuminfo">
+									</div>
+									<div id ="tracklist">
+										<ul>
+											<li>
+												<label id = "trackname">Creep</label><label id="length">2:23</label><label id ="popularity">68%</label>
+												<label id = "trackname">Zwarte pieten stijl</label><label id="length">5:23</label><label id =" popularity">99%</label>
+												<label id = "trackname">Blue dabediebdabeday</label><label id="length">1:11</label><label id =" popularity">100%</label>
+											</li>
+										</ul>
+									</div>
+								</div>
+							</form>
+						</div>
+					</div>
+					<div class="row-fluid">
+						<div id = "friendslistened" class="span12">
+							<form method = "post" onsubmit ="return false;">
+								<fieldset>
+									<legend>
+										Friends
+									</legend>
+								</fieldset>
+								<ul>
+									<li class="span4">
+										<div class="row-fluid">
+											<div class="span4">
+												<label>Name</label>
+											</div>
+											<div class="span8">
+												<label>Mysteryland</label>
+											</div>
+										</div>
+										<div class="row-fluid">
+											<div class="span4">
+												<label>Date</label>
+											</div>
+											<div class="span8">
+												<label>26-11-2012</label>
+											</div>
+										</div>
+										<div class="row-fluid">
+											<div class="span4">
+												<label>Place</label>
+											</div>
+											<div class="span8">
+												<label>Antwerpen</label>
+											</div>
+										</div>
+										<div class="row-fluid">
+											<div class="span4">
+												<label>Costs</label>
+											</div>
+											<div class="span8">
+												<label>125,00</label>
+											</div>
+										</div>	
+									</li>
+								</ul>
+							</form>
+						</div>
+					</div>
+					<div class="row-fluid">
+						<div id = "concertartist" class="span12">
+							<form method = "post" onsubmit ="return false;">
+								<fieldset>
+									<legend>
+										Concerts
+									</legend>
+								</fieldset>
+								<ul>
+									<li class="span4">
+										<div class="row-fluid">
+											<div class="span4">
+												<label>Name</label>
+											</div>
+											<div class="span8">
+												<label>Mysteryland</label>
+											</div>
+										</div>
+										<div class="row-fluid">
+											<div class="span4">
+												<label>Date</label>
+											</div>
+											<div class="span8">
+												<label>26-11-2012</label>
+											</div>
+										</div>
+										<div class="row-fluid">
+											<div class="span4">
+												<label>Place</label>
+											</div>
+											<div class="span8">
+												<label>Antwerpen</label>
+											</div>
+										</div>
+										<div class="row-fluid">
+											<div class="span4">
+												<label>Costs</label>
+											</div>
+											<div class="span8">
+												<label>125,00</label>
+											</div>
+										</div>	
+									</li>
+									<li class="span4">
+										<div class="row-fluid">
+											<div class="span4">
+												<label>Name</label>
+											</div>
+											<div class="span8">
+												<label>Mysteryland</label>
+											</div>
+										</div>
+										<div class="row-fluid">
+											<div class="span4">
+												<label>Date</label>
+											</div>
+											<div class="span8">
+												<label>26-11-2012</label>
+											</div>
+										</div>
+										<div class="row-fluid">
+											<div class="span4">
+												<label>Place</label>
+											</div>
+											<div class="span8">
+												<label>Antwerpen</label>
+											</div>
+										</div>
+										<div class="row-fluid">
+											<div class="span4">
+												<label>Costs</label>
+											</div>
+											<div class="span8">
+												<label>125,00</label>
+											</div>
+										</div>	
+									</li>
+									<li class="span4">
+										<div class="row-fluid">
+											<div class="span4">
+												<label>Name</label>
+											</div>
+											<div class="span8">
+												<label>Mysteryland</label>
+											</div>
+										</div>
+										<div class="row-fluid">
+											<div class="span4">
+												<label>Date</label>
+											</div>
+											<div class="span8">
+												<label>26-11-2012</label>
+											</div>
+										</div>
+										<div class="row-fluid">
+											<div class="span4">
+												<label>Place</label>
+											</div>
+											<div class="span8">
+												<label>Antwerpen</label>
+											</div>
+										</div>
+										<div class="row-fluid">
+											<div class="span4">
+												<label>Costs</label>
+											</div>
+											<div class="span8">
+												<label>125,00</label>
+											</div>
+										</div>	
+									</li>
+								</ul>
+							</form>
+						</div>
+					</div>
+				</div>
+				<div class = "span4">
+					<div class="row-fluid">
+						<div id = "profiel" class="span12">
+							<form method = "post" onsubmit ="return false;">
+								<fieldset>
+									<legend>
+										Profile
+									</legend>
+								</fieldset>
+								<ul>
+									<li>
+										<div class="row-fluid">
+											<div class="span4">
+												<label>Name</label>
+											</div>
+											<div class="span8">
+												<label>Radiohead</label>
+											</div>
+										</div>
+										<div class="row-fluid">
+											<div class="span4">
+												<label>Listening</label>
+											</div>
+											<div class="span8">
+												<label>Creep</label>
+											</div>
+										</div>
+										<div class="row-fluid">
+											<div class="span4">
+												<label>Favourite Artist</label>
+											</div>
+											<div class="span8">
+												<label>Noisecontrollers</label>
+											</div>
+										</div>
+										<div class="row-fluid">
+											<div class="span4">
+												<label>Favourite Genre</label>
+											</div>
+											<div class="span8">
+												<label>Dubstep</label>
+											</div>
+										</div>	
+									</li>
+								</ul>
+							</form>
+						</div>
+					</div>
+					<div class="row-fluid">
+						<div id = "vrienden" class="span12">
+							<form method = "post" onsubmit ="return false;">
+								<fieldset>
+									<legend>
+										Friends
+									</legend>
+								</fieldset>
+								<ul>
+									<li>
+										<div class="row-fluid">
+											<div class="span4">
+												<label>Name</label>
+											</div>
+											<div class="span8">
+												<label>Radiohead</label>
+											</div>
+										</div>
+										<div class="row-fluid">
+											<div class="span4">
+												<label>Listening</label>
+											</div>
+											<div class="span8">
+												<label>Creep</label>
+											</div>
+										</div>
+										<div class="row-fluid">
+											<div class="span4">
+												<label>Favourite Artist</label>
+											</div>
+											<div class="span8">
+												<label>Noisecontrollers</label>
+											</div>
+										</div>
+										<div class="row-fluid">
+											<div class="span4">
+												<label>Favourite Genre</label>
+											</div>
+											<div class="span8">
+												<label>Dubstep</label>
+											</div>
+										</div>	
+									</li>
+								</ul>
+							</form>
+						</div>
+					</div>					
+				</div>	
+			</div>
+		</div>
+	    <!-- Alle Javascript plugins -->
+	    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+		<script src="script/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+		<script src = "script.js" type="text/javascript"></script>
+	</body>
+</html>
